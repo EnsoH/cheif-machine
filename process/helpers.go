794b085390893c7b87bfe2435fdf6d314f167073
ssq0-0 <snikers1kitkat2@gmail.com>
2025-03-04 19:36:34 +0300

    Add log function

--- process/helpers.go
+++ process/helpers.go
@@ -1,6 +1,9 @@
 package process
 
-import "cw/models"
+import (
+	"cw/logger"
+	"cw/models"
+)
 
 func getTokenSums(withdrawArr []models.WithdrawAction) map[string]float64 {
 	sums := map[string]float64{}
@@ -8,4 +11,11 @@ func getTokenSums(withdrawArr []models.WithdrawAction) map[string]float64 {
 		sums[act.Currency] += act.Amount
 	}
 	return sums
+}
+
+func loggingActions(actions []models.WithdrawAction) {
+	for _, act := range actions {
+		logger.GlobalLogger.Infof("[%s] | %s | | %s | | %.8f|", act.Address, act.Chain, act.Currency, act.Amount)
+	}
+	return
 }
